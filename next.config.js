36d6ad18209f6ff0de8bdf57c46a934dd91628db
Shinosuka <shinosukadc123@gmail.com>
2025-08-20 14:43:56 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "unavatar.io" },
+      { protocol: "https", hostname: "unavatar.vercel.app" },
+      { protocol: "https", hostname: "api.dicebear.com" },
+    ],
+  },
+};
+module.exports = nextConfig;
